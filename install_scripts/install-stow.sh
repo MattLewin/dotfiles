be485e7ad46dec6ef11fa8b59e594b547934930f
Matt Lewin <matt@mogroups.com>
2019-06-30 12:18:26 -0400

    Update stow install script to always install the most recently released stow

--- install_scripts/install-stow.sh
+++ install_scripts/install-stow.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -o nounset
 
-curl -fsSL http://ftpmirror.gnu.org/stow/stow-2.3.0.tar.gz | tar xzf -
+curl -fsSL http://ftpmirror.gnu.org/stow/stow-latest.tar.gz | tar xzf -
 CWD=$(pwd)
-cd stow-2.3.0 || ( echo "Unable to find untarred directory 'stow-2.3.0'" 1>&2 ; exit 1 )
+cd stow-* || ( echo "Unable to find untarred 'stow' directory" 1>&2 ; exit 1 )
 echo "Installing required CPAN modules"
 cpan install Log::Log4perl >/dev/null || ( echo "cpan failed to install Log::Log4perl. Is cpan installed?" 1>&2 ; exit 1)
 cpan install inc:latest Clone Hash::Merge Clone::Choose Test::More Test::Output >/dev/null || ( echo "cpan failed to install Test::More and Test::Output" 1>&2 ; exit 1 )
@@ -13,4 +13,4 @@ set -o errexit
 
 ./configure && make
 make install
-cd "$CWD" && rm -rf stow-2.3.0
+cd "$CWD" && rm -rf stow-*
