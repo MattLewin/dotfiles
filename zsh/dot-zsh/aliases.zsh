24cb7e4a3f21b25a3e0d6b97714d83aac8374354
Matt Lewin <matt@mogroups.com>
2019-12-10 17:02:29 -0500

    aliases: fix tmo_repo_install to not screw up bundle installation

--- zsh/dot-zsh/aliases.zsh
+++ zsh/dot-zsh/aliases.zsh
@@ -254,7 +254,7 @@ unalias gl
 #
 if [ "$(hostname)" = "bos075" ]; then
     function tmo_repo_install() {
-        bundle install --path ${HOME}/.bundle
+        bundle install
         git submodule update --init --recursive
         [ -e "Podfile" ] && bundle exec pod install $@
     }
