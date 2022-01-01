e91b64e79da6b7bdaf8966fc4a56b21f2b57e16a
Matt Lewin <matt@mogroups.com>
2022-01-01 15:10:13 -0500

    Ensure ssh identities are added to the agent at launch
    - fix: no longer create junk links if no plist files exist
    - fix: ensure launch_agents dir always exists
    - feature: add all identities to ssh agent at login

--- install_scripts/install-launch-agents.sh
+++ install_scripts/install-launch-agents.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env zsh
+
+#
+# NOTE: this script assumes we are on macOS. Lame!
+#
+
 set -o errexit
 set -o nounset
 
 AGENTS_DIR="$(pwd)/launch_agents"
 LIBRARY_LAUNCHAGENTS="${HOME}/Library/LaunchAgents"
 
-for fq_agent in "${AGENTS_DIR}/*.plist"(N.)
+for fq_agent in ${AGENTS_DIR}/*.plist(N.)
 do
     agent="$(basename -s .plist ${fq_agent})"
-    echo "Installing launch agent: ${fq_agent}"
+    print "Installing launch agent: ${fq_agent}"
     ln -sf "${fq_agent}" "${LIBRARY_LAUNCHAGENTS}/"
+    # NOTE: this is using the obsolete 'load' and 'start' instead of
+    #       'boostrap', because I couldn't make 'bootstrap' work
     launchctl load "${LIBRARY_LAUNCHAGENTS}/${agent}.plist"
     launchctl start "${agent}"
 done
