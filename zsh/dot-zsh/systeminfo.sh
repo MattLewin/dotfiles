df3778ee49cc7eadba92a1e1054945b78ce91891
Matt Lewin <matt@mogroups.com>
2025-09-16 23:06:29 -0700

    Replace systeminfo.sh with moden, POSIX compliant one

--- zsh/dot-zsh/systeminfo.sh
+++ zsh/dot-zsh/systeminfo.sh
@@ -1,58 +1,48 @@
-#!/usr/bin/env zsh
+# Intended to be *sourced*, not executed.
+# Exports: OS KERNEL MACH ARCH DIST REV PSEUDONAME DistroBasedOn OS_STR
 
-####################################################################
-# Get System Info
-####################################################################
-_getsysteminfo() {
-    # typeset -l forces the envionment variable to be lowercase
-    typeset -xl OS=$(uname -s) # operating system name (NOTE: the -x is required or it will not export later)
-    KERNEL=$(uname -r) # operating system kernel version
-    MACH=$(uname -m) # machine hardware name
-    ARCH=$(uname -p 2>/dev/null || uname -m) # processor type or machine hardware name, if uname -p is unavailable
+# If not sourced, $0 is this file; sourced, $0 is the shell.
+case "$0" in *systeminfo.sh) echo "Source this file, don't execute it." >&2; return 1 2>/dev/null || exit 1;; esac
 
-    if [ "${OS}" = "windowsnt" ]; then
-        OS=windows
-    elif [ "${OS}" = "darwin" ]; then
-        OS_STR=$(uname -v) # operating system version string
+# Normalize and gather basics
+OS=$(uname -s 2>/dev/null | tr '[:upper:]' '[:lower:]'); : "${OS:=unknown}"
+KERNEL=$(uname -r 2>/dev/null || printf '')
+MACH=$(uname -m 2>/dev/null || printf '')
+ARCH=$( (uname -p 2>/dev/null) || printf '%s' "$MACH" )
+
+case "$OS" in
+  darwin)
+    if command -v sw_vers >/dev/null 2>&1; then
+      # Example: "macOS 14.6.1"
+      OS_STR="$(sw_vers -productName) $(sw_vers -productVersion)"
     else
-        OS=$(uname)
-        if [ "${OS}" = "sunos" ]; then
-            OS=solaris
-            OS_STR="${OS} (${ARCH} $(uname -v))"
-        elif [ "${OS}" = "aix" ]; then
-            OS_STR="${OS} $(oslevel) ($(oslevel -r))"
-        elif [ "${OS}" = "linux" ]; then
-            if [ -f /etc/redhat-release ]; then
-                DistroBasedOn='RedHat'
-                DIST=$(cat /etc/redhat-release | sed 's/\ release.*//')
-                PSEUDONAME=$(cat /etc/redhat-release | sed 's/.*(//' | sed 's/)//')
-                REV=$(cat /etc/redhat-release | sed 's/.*release\ //' | sed 's/\ .*//')
-            elif [ -f /etc/SuSE-release ]; then
-                DistroBasedOn='SuSe'
-                PSEUDONAME=$(cat /etc/SuSE-release | tr "\n" ' ' | sed 's/VERSION.*//')
-                REV=$(cat /etc/SuSE-release | tr "\n" ' ' | sed 's/.*=\ //')
-            elif [ -f /etc/mandrake-release ]; then
-                DistroBasedOn='Mandrake'
-                PSEUDONAME=$(cat /etc/mandrake-release | sed 's/.*(//' | sed 's/)//')
-                REV=$(cat /etc/mandrake-release | sed 's/.*release\ //' | sed 's/\ .*//')
-            elif [ -f /etc/debian_version ]; then
-                DistroBasedOn='Debian'
-                if [ -f /etc/lsb-release ]; then
-                    DIST=$(cat /etc/lsb-release | grep '^DISTRIB_ID' | awk -F= '{ print $2 }')
-                    PSEUDONAME=$(cat /etc/lsb-release | grep '^DISTRIB_CODENAME' | awk -F= '{ print $2 }')
-                    REV=$(cat /etc/lsb-release | grep '^DISTRIB_RELEASE' | awk -F= '{ print $2 }')
-                fi
-            fi
-            if [ -f /etc/UnitedLinux-release ]; then
-                DIST="${DIST}[$(cat /etc/UnitedLinux-release | tr "\n" ' ' | sed 's/VERSION.*//')]"
-            fi
-        fi
+      OS_STR="$(uname -srvmo 2>/dev/null)"
     fi
+    DIST="macos"
+    REV="${OS_STR##* }"   # version tail; fine if sw_vers not present
+    PSEUDONAME=""
+    DistroBasedOn="bsd"
+    ;;
 
-    # `typeset -gxlr` exports the variables as read-only and lowercase in the global context
-    typeset -gxlr ARCH DIST DistroBasedOn KERNEL MACH OS PSEUDONAME REV
-    typeset -gxr OS_STR # We want to preserve the case of the OS string
-}
+  linux)
+    if [ -r /etc/os-release ]; then
+      # shellcheck disable=SC1091
+      . /etc/os-release
+      DIST=${ID:-linux}
+      REV=${VERSION_ID:-}
+      PSEUDONAME=${VERSION_CODENAME:-}
+      DistroBasedOn=${ID_LIKE:-}
+      OS_STR=${PRETTY_NAME:-"Linux $KERNEL"}
+    else
+      DIST="linux"; REV=""; PSEUDONAME=""; DistroBasedOn=""
+      OS_STR="Linux $KERNEL"
+    fi
+    ;;
 
-_getsysteminfo
-unset -f _getsysteminfo
+  *)
+    DIST="$OS"; REV=""; PSEUDONAME=""; DistroBasedOn=""
+    OS_STR="$(uname -srvmo 2>/dev/null)"
+    ;;
+esac
+
+export OS KERNEL MACH ARCH DIST REV PSEUDONAME DistroBasedOn OS_STR
