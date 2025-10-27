05fed9a2eae97bc5d9cf4428e40402af511ca18e
Matt Lewin <matt@mogroups.com>
2025-10-27 16:47:27 -0700

    fish config: activate mise for interactive shells

--- misc/dot-config/fish/config.fish
+++ misc/dot-config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     eval (direnv hook fish)
     starship init fish | source
     zoxide init fish | source
+    mise activate fish | source
 
     # Make zoxide case-insensitive
     set -x _ZO_CASE 0
