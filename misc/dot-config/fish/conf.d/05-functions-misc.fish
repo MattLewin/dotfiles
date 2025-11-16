7fb997a3dd8c03ce6ac70635e96530eaf1ea1c11
Matt Lewin <matt@mogroups.com>
2025-11-15 21:54:02 -0800

    Make 'detect_ping_spikes' a proper fish function and move it to conf.d

---
+++ misc/dot-config/fish/conf.d/05-functions-misc.fish
@@ -0,0 +1,18 @@
+function detect_ping_spikes
+  # Ping every 5 seconds and highlight spikes over 100 ms
+  ping -i 5 -D google.com | awk -v thr=100 '
+  /^\[[0-9]+\.[0-9]+\]/ {
+    if (match($0, /^\[([0-9]+)\./, t) && match($0, /time=([0-9.]+) ms/, m)) {
+      ts = t[1] + 0
+      ms = m[1] + 0
+      human = strftime("%Y-%m-%d %H:%M:%S", ts)
+      if (ms > thr)
+        printf("\033[31m[%s] %.1f ms\033[0m\n", human, ms)
+      else
+        printf("[%s] %.1f ms\n", human, ms)
+      fflush()
+      next
+    }
+  }
+  { print; fflush() }'
+end
