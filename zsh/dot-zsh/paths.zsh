dae42df34a0a9d31b570ee4a56a087d3775d75a6
Matt Lewin <matt@mogroups.com>
2026-02-12 16:29:07 -0800

    rely on `typeset -U` to prevent duplicate paths instead of my function

--- zsh/dot-zsh/paths.zsh
+++ zsh/dot-zsh/paths.zsh
@@ -48,19 +48,3 @@ if [[ -x $hb_ruby ]]; then
 fi
 
 export PATH
-
-function remove_duplicate_paths() {
-    if [ -n "$PATH" ]; then
-        old_PATH=$PATH:; unset PATH;
-        while [ -n "$old_PATH" ]; do
-            x=${old_PATH%%:*}       # the first remaining entry
-            case $PATH: in
-                *:"$x":*) ;;          # already there
-                *) PATH=$PATH:$x;;    # not there yet
-            esac
-            old_PATH=${old_PATH#*:}
-        done
-        PATH=${PATH#:}
-        unset old_PATH x
-    fi
-}
