ab846fec8468471268975161e7c64023e1bb4719
Matt Lewin <matt@mogroups.com>
2026-05-28 10:38:24 -0700

    brew: add poppler (PDF tools) and an alias to preserve layout

--- zsh/dot-zsh/aliases.zsh
+++ zsh/dot-zsh/aliases.zsh
@@ -8,6 +8,7 @@ alias hgrep='fc -il 0 | grep'
 alias hrg='fc -il 0 | rg'
 alias nslookup6='nslookup -querytype=AAAA'
 alias pbc='clipcopy'
+alias pdftotext='pdftotext -layout'
 alias ping="ping -c 5"
 alias ping6='ping6 -c 5'
 alias stat='zstat -s'
